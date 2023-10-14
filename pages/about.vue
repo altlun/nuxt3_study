b31dd9bbf0226bc1fbd91ab74bbbd4cfd4d8ca0f
altlun <altlundriver440@gmail.com>
2023-10-14 13:25:22 +0900

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1>About Page</h1>
+        <NuxtLink to="/">Root</NuxtLink>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1>
+            Hello nuxt and dummyjson!!
+        </h1>
+        <NuxtLink to="/about">About</NuxtLink>
+    </div>
+</template>
